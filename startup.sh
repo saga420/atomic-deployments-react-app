207ce3ab0db2f0f64a9c062754e0139137ecbe9f
Fake Name <i_dont_want_share_email_address@with-you.local>
2020-06-13 22:56:25 +0800

    startup.sh

---
+++ startup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/aaronjwang/atomic-deployments-react-app.git
+cd atomic-deployments-react-app
+python3 -m pip install --user virtualenv
+python3 -m venv env && source env/bin/activate
+pip3 install -r requirements.txt
